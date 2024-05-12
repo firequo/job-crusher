d4ac96435f498bca22df89ba0e4f54d55fc42863
firequo <apasov2@gmail.com>
2024-05-12 01:08:12 -0400

    multiple requests to adzuna fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,9 @@ let jobs = [
     'help desk',
     'technical support',
     'project management',
-    'operations',
+    'operations'
 ];
+let avgSalaries = require('./average_salaries.json');
 let apiKeyInfo = require('./keys.json');
 const api_key = apiKeyInfo.apiKey;
 const api_id = apiKeyInfo.apiId;
@@ -20,9 +22,7 @@ app.get('/', (req, res) => {
     res.sendFile('public/Home_Page.html', {root: __dirname});
 });
 app.get('/salary/top', (req, res) => {
-    let retval = getDataFile();
-    console.log(retval);
-    res.send(retval);
+    res.send(avgSalaries);
 });
 
 app.listen(port, () => {
@@ -32,23 +32,22 @@ app.listen(port, () => {
 // use this as template for requests
 function sample_http_request(){
     let url = 'https://whatever.com';
-        https.get(url, (res) => {
-            let data = [];
-            res.on('data', chunk => {
-                data.push(chunk);
-            });
-            res.on('end', () => {
-                const json = JSON.parse(Buffer.concat(data).toString());
-                // change here
-
-            });
-        }).on('error', (e) => {
-            console.error(e);
+    https.get(url, (res) => {
+        let data = [];
+        res.on('data', chunk => {
+            data.push(chunk);
         });
+        res.on('end', () => {
+            const json = JSON.parse(Buffer.concat(data).toString());
+            // change here
+
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    });
 }
 
 // mock request
-// not a fan of json
 let testfile = require('./ex_resp.json');
 function getDataFile(){
     let retval = {};
@@ -58,41 +57,62 @@ function getDataFile(){
     retval[jobs[0]] = meanSalary;
     return retval;
 }
+let averageSalaries = {};
+let today = new Date();
+// if(today.getHours()  == 1 && today.getMinutes() == 0){
+//     //nodemon makes this an infinite loop, dont run this please ever
+//     getDataHttp(0, averageSalaries, saveSalaries);
+// }
+function saveSalaries(json){
+    var string = JSON.stringify(json);
+    fs.writeFileSync('average_salaries.json', string, 'utf8', );
+}
 // this function makes a request for each job in the list up top, so prob gonna set up a syncer to db once a day or smth to not spam
+// code above does the full call and saves it to a file when the server starts
+// do NOT use with nodemon
 // PLEASE DO NOT TEST USING THIS 
 // it will fuck 
-function getDataHttp(){
-    let retval = {};
-    for(let i = 0; i < jobs.length; i++){
-        const job = jobs[i];
-        const url = `https://api.adzuna.com/v1/api/jobs/us/histogram?app_id=${api_id}&app_key=${api_key}&what=${job}&content-type=application/json`;
+// cursed
+// adzuna does not allow concurrent requests, so have to do it this way smh
+function getDataHttp(i, retval, callback){
+    const job = jobs[i];
+    const url = `https://api.adzuna.com/v1/api/jobs/us/histogram?app_id=${api_id}&app_key=${api_key}&what=${job}&content-type=application/json`;
 
-        https.get(url, (res) => {
-            let data = [];
-            res.on('data', chunk => {
-                data.push(chunk);
-            });
-            res.on('end', () => {
-                const json = JSON.parse(Buffer.concat(data).toString());
+    https.get(url, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        let data = [];
+        res.on('data', chunk => {
+            data.push(chunk);
+        });
+        res.on('end', () => {
+            const newdata = Buffer.concat(data).toString();
+
+            if(res.statusCode == 200){
+                const json = JSON.parse(newdata);
                 console.log(json);
                 let meanSalary = calcMeanSalary(json.histogram);
                 console.log(`job: ${job} mean salary: ${meanSalary}`);
                 retval[job] = meanSalary;
-            });
-        }).on('error', (e) => {
-            console.error(e);
+                if(i == jobs.length - 1){
+                    console.log(retval);
+                    callback(retval);
+                } else {
+                    getDataHttp(i + 1, retval, callback);
+                }
+            }
         });
-    }
-    return retval;
+    }).on('error', (e) => {
+        console.error(e);
+    });
 }
 function calcMeanSalary(histogram){
-    console.log(histogram);
     let sum = 0;
     let counter = 0;
     // other ways of looping over this were straight up not working, no idea why
     // boomer loops win again ig
     let keys = Object.keys(histogram);
-    console.log(keys);
     for(let i = 0; i < keys.length; i++){
         let salary = keys[i];
         let amount = Number(histogram[salary]);
@@ -100,7 +120,5 @@ function calcMeanSalary(histogram){
         sum += Number(salary) * count;
         counter += count;
     }
-    console.log(counter);
-    console.log(sum);
     return sum / counter;
 }
